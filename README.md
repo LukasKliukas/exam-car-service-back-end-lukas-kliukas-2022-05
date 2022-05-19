1c8d9f4a3672177b1375ee03934e1584719fb992
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-19 13:21:37 +0300

    First init

---
+++ README.md
@@ -0,0 +1,8 @@
+## install:
+
+1. npm i
+2. copy .env example to .env and enter your values
+
+## how to start:
+
+1. npm start
---
+++ src/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mysql = require('mysql2/promise');
+
+const PORT = process.env.SERVER_PORT || 5000;
+
+const app = express();
+
+//MiddleWare
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.get('/', (request, response) => {
+  response.send('hello from back end');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
