eb10b66beaf2d194ece22c8ceede0afe9e8fd97e
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-22 17:07:21 +0300

    AuthController created

---
+++ src/controllers/authController.js
@@ -0,0 +1,35 @@
+const { insertUser, findUserByEmail } = require('../model/authModel');
+const { successResponce, failResponce } = require('../utils/dbHelpers');
+const { hashPass, verifyHash, generateJwtToken } = require('../utils/helper');
+
+async function register(req, res) {
+  const { email, password } = req.body;
+  const hashedPassword = hashPass(password);
+  const insertResult = await insertUser(email, hashedPassword);
+
+  return insertResult === false
+    ? failResponce(res)
+    : successResponce(res, 'user created');
+}
+async function login(req, res) {
+  const { email, password } = req.body;
+
+  const findResults = await findUserByEmail(email);
+  if (findResults === false) return failResponce(res);
+  if (!findResults.length)
+    return failResponce(res, 'email or pass not match 1');
+
+  const foundUserObj = findResults[0];
+
+  if (!verifyHash(password, foundUserObj)) {
+    return failResponce(res, 'email or pass not match 2');
+  }
+
+  const token = generateJwtToken(foundUserObj);
+  successResponce(res, token);
+}
+
+module.exports = {
+  register,
+  login,
+};
