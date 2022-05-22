75ce3a2e7214a1474ee4aefb5035914e553fa14d
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-22 17:02:50 +0300

    AuthModel Created

---
+++ src/model/authModel.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql2/promise');
+const dbConfig = require('../dbConfig');
+const tableName = 'users';
+
+async function insertUser(email, pass) {
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    const sql = `
+    INSERT INTO ${tableName} (email,password)
+    VALUES (?,?)
+    `;
+    const [insertResult] = await conn.execute(sql, [email, pass]);
+    await conn.end();
+    return insertResult;
+  } catch (error) {
+    console.log('insertUser ===', error);
+    return false;
+  }
+}
+async function findUserByEmail(email) {
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    const sql = `
+    SELECT * FROM ${tableName}
+    WHERE email = ?
+    `;
+    const [userFoundResult] = await conn.execute(sql, [email]);
+    await conn.end();
+    return userFoundResult;
+  } catch (error) {
+    console.log('findUserByEmail ===', error);
+    return false;
+  }
+}
+module.exports = {
+  insertUser,
+  findUserByEmail,
+};
